1829070336f8f63f89a37889ce4cefdc9bbd0938
SureshGudibanda <sureshgudibanda@integramicro.co.in>
2025-11-09 11:23:34 +0530

    fc

---
+++ src/main/java/com/demo/controller/DemoController.java
@@ -0,0 +1,24 @@
+package com.demo.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+
+public class DemoController {
+
+	public DemoController() {
+
+		System.out.println("constructor");
+	}
+
+	@GetMapping("/demo")
+	public String demo() {
+
+		System.out.println("am demo");
+		return "demo";
+	}
+
+}
